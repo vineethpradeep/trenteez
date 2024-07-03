2e6a4c361caf959516b467691249672edcede779
vineethpradeep <vineethpadeeps@gmail.com>
2024-07-03 02:00:31 +0100

    initial deploy

---
+++ readme.md
@@ -0,0 +1,31 @@
+# Trenteez - Custom T-Shirt Graphic Design
+
+The Trenteez is an endeavor of a business that focuses on the creation and production of unique graphic designs for T-shirts. An online customization tool is being developed as part of the and is expected to play a key role in future upgrades. This prioritizes responsive design to guarantee smooth user experiences across all platforms.
+
+## Features
+
+- **Unique Graphic Designs**: Focuses on the creation and production of unique T-shirt designs.
+- **Online Customization Tool**: An online tool for customizing T-shirt designs is in development and will be a key feature in future updates.
+- **Responsive Design**: Ensures a smooth user experience across all platforms.
+- **Front-End Technologies**: Built using JavaScript, HTML, CSS, and CSS animations for a dynamic and interactive interface.
+
+## Future Development
+
+- **Enhanced Customization**: Users will be able to choose and personalize T-shirt designs online.
+- **Integrated Payment Gateway**: Secure payment options will be integrated for seamless transactions.
+- **Order Confirmation and Delivery**: The system will handle order confirmations and coordinate deliveries.
+
+## Technologies Used
+
+- **JavaScript**
+- **HTML**
+- **CSS**
+- **CSS Animations**
+
+## Installation
+
+1. **Clone the Repository**:
+   ```bash
+   git clone https://github.com/vineethpradeep/trenteez.git
+   cd trenteez
+   ```
---
+++ script.js
@@ -0,0 +1,205 @@
+"use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const header = document.querySelector(".header");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function (e) {
+  e.preventDefault();
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+const h1 = document.querySelector("h1");
+const tabContainer = document.querySelector(".operations__tab-container");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabContents = document.querySelectorAll(".operations__content");
+
+tabContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  if (!clicked) return;
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  tabContents.forEach((tabContent) =>
+    tabContent.classList.remove("operations__content--active")
+  );
+  clicked.classList.add("operations__tab--active");
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+const nav = document.querySelector(".nav");
+const handleOver = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const sibilings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    sibilings.forEach((el) => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+      logo.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener("mouseover", handleOver.bind(0.5));
+nav.addEventListener("mouseout", handleOver.bind(1));
+
+const navHeight = header.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+const allSection = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSection.forEach(function (section) {
+  sectionObserver.observe(section);
+});
+
+const imgTarget = document.querySelectorAll("img[data-src]");
+const imgLoader = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(imgLoader, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+imgTarget.forEach((img) => imgObserver.observe(img));
+
+function slider() {
+  const dotContainer = document.querySelector(".dots");
+  const slides = document.querySelectorAll(".slide");
+
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  slides.forEach(function (slide, i) {
+    slide.style.transform = `translateX(${100 * i}%)`;
+  });
+  let currentSlide = 0;
+  let maxSlide = slides.length;
+  const btnRight = document.querySelector(".slider__btn--right");
+  const btnLeft = document.querySelector(".slider__btn--left");
+
+  const activeDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  const goToSlide = function (currentSlide) {
+    slides.forEach(function (slide, i) {
+      slide.style.transform = `translateX(${100 * (i - currentSlide)}%)`;
+    });
+  };
+
+  const nextSlide = function () {
+    if (currentSlide === maxSlide - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    goToSlide(currentSlide);
+    activeDot(currentSlide);
+  };
+
+  const prevSlide = function () {
+    if (currentSlide === 0) {
+      currentSlide = maxSlide - 1;
+    } else {
+      currentSlide--;
+    }
+    goToSlide(currentSlide);
+    activeDot(currentSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+    activeDot(0);
+  };
+  init();
+
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      nextSlide();
+    }
+    e.key === "ArrowLeft" && prevSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activeDot(slide);
+    }
+  });
+}
+slider();
